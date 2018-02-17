e0cb1ed5ab6974ca33f1c57f72b4d0a10856aad7
Tebogo Sewape <josiejsoi@ymail.com>
2018-02-17 17:41:39 +0200

    Reverce

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
 
             event( new \App\Events\LatestCryptoPrices( [ 'btc' => $btc, 'eth' => $eth ] ) ) ;
 
-        })->everyMinute() ;
+        })->everyFiveMinutes() ;
     }
 
     protected function commands()
